f04719108bbdbf99aad67752da6e653b3cfccaf2
Ruan Blotta <ruanblotta@gmail.com>
2024-02-27 16:22:14 -0300

    Orientação a Objetos em C#: Atividades Complementares

---
+++ projetos-backend/Atividades/Orientação a Objetos/Aula 01 - Ativodade Complementar 2/mediaSalario/mediaSalario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mediaSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FolhaPagamento A = new FolhaPagamento();
+            FolhaPagamento B = new FolhaPagamento();
+            
+            Console.WriteLine("Informe o nome do funcionário 01: ");
+            A.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o salário do funcionário 01: ");
+            A.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o nome do funcionário 02: ");
+            B.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o salário do funcionário 02: ");
+            B.Salario = double.Parse(Console.ReadLine());
+
+            double MediaSal;
+
+            MediaSal = (A.Salario + B.Salario) / 2;
+
+            Console.WriteLine("A media salarial entre os funcionário é de R$:" + MediaSal);
+        }
+    }
+}
+
+
+

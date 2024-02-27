fc2e51d0da12b0d6f16e9e026ccc408686cadbed
Ruan Blotta <ruanblotta@gmail.com>
2024-02-27 15:53:13 -0300

    Projeto Cinema: Atualização 01

--- projetos-frontend/projeto-cinema/script/script.js
+++ projetos-frontend/projeto-cinema/script/script.js
@@ -1,3 +1,36 @@
-function swapColor(color:fcb000) {
-    document.body.style.backgroundColor = color: #fcb000;
+//função Dark Mode
+function swapColorBlack() {
+    if (document.body.style.backgroundColor == 'black') {
+        dicument.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    } else {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }
 }
+
+//função Light Mode
+function swapColorWhite() {
+    if (document.body.style.backgroundColor == 'white') {
+        dicument.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
+
+function addText() {
+    var newParagraphy = document.createElement("p")
+    var text = "O Mágico de OZ - 20:00 - Hoje"
+
+    newParagraphy.textContent = text;
+
+    document.body.appendChild(newParagraphy);
+}
+
+addText();
+
+// document.querySelectorAll('h2').forEach(function(h2)){
+//     h2.style.color = "#fcb000";   
+// });
